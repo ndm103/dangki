903805b54ec68d4d7f1ad18f487b036e781759da
ndm103 <111834158+ndm103@users.noreply.github.com>
2023-09-07 09:49:46 +0700

    Update xuly.php

--- dangki/xuly.php
+++ dangki/xuly.php
@@ -1,9 +1,7 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
-// Kết nối cơ sở dữ liệu
 $conn = mysqli_connect('localhost', 'root', '', 'data') or die ('Lỗi kết nối'); mysqli_set_charset($conn, "utf8");
 
-// Dùng isset để kiểm tra Form
 if(isset($_POST['dangky'])){
 $username = trim($_POST['username']);
 $password = trim($_POST['password']);
@@ -24,18 +22,14 @@ if (empty($password)) {
 array_push($errors, "Two password do not match"); 
 }
 
-// Kiểm tra username hoặc email có bị trùng hay không
 $sql = "SELECT * FROM member WHERE username = '$username' OR email = '$email'";
 
-// Thực thi câu truy vấn
 $result = mysqli_query($conn, $sql);
 
-// Nếu kết quả trả về lớn hơn 1 thì nghĩa là username hoặc email đã tồn tại trong CSDL
 if (mysqli_num_rows($result) > 0)
 {
 echo '<script language="javascript">alert("Bị trùng tên hoặc chưa nhập tên!"); window.location="register.php";</script>';
 
-// Dừng chương trình
 die ();
 }
 else {
